2866a837810357e216efff8cae95ec5111cb279c
narendra <narendramiriyala450@achiever-75.local>
2018-05-08 16:34:05 +0530

    nari

---
+++ nariApp/nariApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nariApp
+//
+//  Created by narendra on 5/8/18.
+//  Copyright © 2018 narendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
